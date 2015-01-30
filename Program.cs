e851d09744217d3ac17178177b42ad43b1cbe8f1
almarrihadi <alkatm8@hotmail.com>
2015-01-29 21:17:27 -0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+ class Program
+ {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+ }
+
